17c2ae00656af5e30590521815bddff58a33dd2b
hwangsungmin-00 <hwangsungmin_00@naver.com>
2021-09-27 18:53:58 +0900

    CBV로 블로그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model=Post
+    ordering='-pk'
+#    template_name = 'blog/post_list.html'
+# post_list.html
+class PostDetail(DetailView):
+    model=Post
+# post_detail.html
 
-    return render(request, 'blog/index.html',
-                  {
-                      'posts': posts
-                  })
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+#
+#    return render(request, 'blog/post_list.html',
+#                 {
+#                     'posts': posts
+#                 })
 
-def single_post_page(request, pk):
-    post=Post.objects.get(pk=pk)
-
-    return render(request, 'blog/single_post_page.html',
-                  {
-                      'post': post
-                  })
+#def single_post_page(request, pk):
+#   post=Post.objects.get(pk=pk)
+#
+#   return render(request, 'blog/post_detail.html',
+#                {
+#                  'post': post
+#               })
