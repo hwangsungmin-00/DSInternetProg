b9559bd05c9457d9ed78470a9ad8d022b9639c0e
hwangsungmin <hwangsungmin_00@naver.com>
2021-10-27 10:41:14 +0900

    포스트 목록/상세 페이지에 작성자 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 from django.test import TestCase, Client
 from .models import Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestView(TestCase):
     def setUp(self):
         self.client=Client()
+        self.user_james=User.objects.create_user(username='James', password='somepassword')
+        self.user_trump = User.objects.create_user(username='Trump', password='somepassword')
 
     def navbar_test(self, soup):
         # 네비게이션바가 있다
@@ -49,11 +52,13 @@ class TestView(TestCase):
         #포스트(게시물)이 2개 존재하는 경우
         post_001=Post.objects.create(
             title='첫 번쨰 포스트입니다.',
-            content='Hello World!!! We are the World...'
+            content='Hello World!!! We are the World...',
+            author=self.user_james
         )
         post_002 = Post.objects.create(
             title='두 번쨰 포스트입니다.',
-            content='1등이 전부가 아니잖아요'
+            content='1등이 전부가 아니잖아요',
+            author=self.user_trump
         )
         self.assertEqual(Post.objects.count(), 2)
         #목록페이지를 새롭게 불러와서
@@ -66,13 +71,16 @@ class TestView(TestCase):
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
-
+        self.assertIn(self.user_james.username.upper(), main_area.text)
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
 
     def test_post_detail(self):
         #포스트 하나
         post_001 = Post.objects.create(
             title='첫 번쨰 포스트입니다.',
-            content='Hello World!!! We are the World...'
+            content='Hello World!!! We are the World...',
+            author=self.user_james
+
         )
         #이 포스트의 url이 /blog/1
         self.assertEqual(post_001.get_absolute_url(), '/blog/1')
@@ -94,5 +102,7 @@ class TestView(TestCase):
         #포스트의 내용이 있는가
         self.assertIn(post_001.content, post_area.text)
 
+
+        self.assertIn(self.user_james.username.upper(), post_area.text)
 
 
