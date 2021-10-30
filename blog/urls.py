d18ff9197b0b60211f9fdcba6a9767b5a3922bd6
hwangsungmin <hwangsungmin_00@naver.com>
2021-10-30 21:20:02 +0900

    포스트 목록/상세 페이지에 태그 기능 추가 및 태그페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ urlpatterns=[
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index), # 서버IP/blog
 
-    path('category/<str:slug>', views.category_page),
+    path('tag/<str:slug>', views.tag_page),
+    path('category/<str:slug>', views.category_page), #서버IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
