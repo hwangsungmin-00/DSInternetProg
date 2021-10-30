343f69edfd1c45af3d4d7b9c299fa74e3beab0c4
hwangsungmin <hwangsungmin_00@naver.com>
2021-10-30 22:24:29 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ urlpatterns=[
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index), # 서버IP/blog
 
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page), #서버IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
