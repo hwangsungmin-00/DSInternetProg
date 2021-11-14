1fc63c1e8edf2ebc79be171cf44b013e240ee7ed
hwangsungmin <hwangsungmin_00@naver.com>
2021-11-14 15:13:02 +0900

    댓글을 포스트 상세페이지에 나타내기

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from django.test import TestCase, Client
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -40,6 +40,12 @@ class TestView(TestCase):
         )
         self.post_003.tags.add(self.tag_python)
         self.post_003.tags.add(self.tag_python_kor)
+
+        self.comment_001 = Comment.objects.create(
+            post = self.post_001,
+            author = self.user_trump,
+            content = '첫번째 댓글입니다.'
+        )
     def navbar_test(self, soup):
         # 네비게이션바가 있다
         navbar = soup.nav
@@ -268,4 +274,8 @@ class TestView(TestCase):
 
         self.assertIn(self.user_james.username.upper(), post_area.text)
 
+        comments_area = soup.find('div',id='comment-area')
+        comment_001_area = comments_area.find('div',id='comment-1')
+        self.assertIn(self.comment_001.author.username,comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
 
