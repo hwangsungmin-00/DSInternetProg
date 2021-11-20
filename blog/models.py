e0bba538771805ab0998a5a4689aae831de4200c
hwangsungmin <hwangsungmin_00@naver.com>
2021-11-20 22:49:18 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -70,3 +70,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists() :
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return 'https://doitdjango.com/avatar/id/366/ebfc16b5a820140f/svg/{self.author.email}/'
