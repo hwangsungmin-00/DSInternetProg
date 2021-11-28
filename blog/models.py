681308c46ba1404e1d31e20ee66c6cdb41c36774
hwangsungmin <hwangsungmin_00@naver.com>
2021-11-28 20:55:16 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -58,6 +58,13 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists() :
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return 'https://doitdjango.com/avatar/id/366/ebfc16b5a820140f/svg/{self.author.email}/'
+
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
